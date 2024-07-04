644a2a6c0c386864104b0a110a53c5ae91b77721
valery.kavaleuski <valery.kavaleuski@gmail.com>
2024-07-04 14:11:28 +0300

    main; added blank project

---
+++ test-assignment-genai-apps/test-assignment-genai-apps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test-assignment-genai-apps
+//
+//  Created by Valery Kavaleuski on 4.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
